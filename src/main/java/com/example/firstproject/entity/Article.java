5c28bfb5547a77843911cff353da49bfed10ee42
Kangboom <jkl6147@naver.com>
2024-03-17 21:19:13 +0900

    Initial commit

---
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,33 @@
+package com.example.firstproject.entity;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@ToString
+@NoArgsConstructor
+@Getter
+@AllArgsConstructor
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+
+    public void patch(Article article) {
+        if (article.title != null){
+            this.title = article.title;
+        }
+        if (article.content != null){
+            this.content = article.content;
+        }
+    }
+}
